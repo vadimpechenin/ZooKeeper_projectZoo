2afea5d6100730c8255bba466bba6bf94d0ed876
Vadim Pechenin <vadim.pechenin2015@gmail.com>
2020-11-09 13:45:34 +0400

    First commit zoo of ZooKeeper

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "spark-lab-1"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.zookeeper" % "zookeeper" % "3.5.8"
---
+++ src/main/scala/zoo/Main.scala
@@ -0,0 +1,94 @@
+package zoo
+
+import org.apache.zookeeper._
+// Необходимая библеотека
+import scala.util.Random
+// Реализация класса Animal
+
+case class Animal(name:String, hostPort:String,
+                  root:String, partySize:Integer) extends Watcher {
+  // Инициализация Animal заключается в
+  //установлении соединения с ZooKeeper, определении переменных mutex и
+  //animalPath.
+  val zk = new ZooKeeper(hostPort, 3000, this)
+  val mutex = new Object()
+  val animalPath = root+"/"+name
+
+  // Реализация методов
+  //process интерфейса Watcher.
+  override def process(event: WatchedEvent): Unit = {
+    // код реакции на событие
+    // Блок синхронизации
+    mutex.synchronized {
+      // Простая реакция - вывод на экран события
+      println(s"Event from keeper: ${event.getType}")
+    }
+  }
+
+  // Реализация метода enter
+  def enter():Boolean = {
+    // код создания узла и ожидания у барьера
+    // создание эфимерного узла
+    zk.create(animalPath, Array.emptyByteArray,
+      ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL)
+
+    // Блок, ожидающий появления в корневом узле /zoo всех животных
+    mutex.synchronized {
+      while (true) {
+        val party = zk.getChildren(root, this)
+        if (party.size() < partySize) {
+          println("Waiting for the others.")
+          mutex.wait()
+          println("Noticed someone.")
+        } else {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  // метод удаления эфимерного узла
+  def leave():Unit = {
+    zk.delete(animalPath,-1)
+    println("Deleted")
+  }
+
+  if (zk == null) throw new Exception("ZK is NULL.")
+}
+
+object Main {
+
+  val sleepTime = 100
+
+  def main(args: Array[String]): Unit = {
+    println("Starting animal runner")
+    // Программа ожидает в
+    //качестве аргументов список: имя животного, адрес и порт zookeeper, размер
+    //группы животных
+    val Seq(animalName, hostPort, partySize) =
+      args.toSeq
+    // оздание объекта Animal на основе
+    //параметров: имя животного, адрес и порт zookeeper, путь к корневому узлу
+    //для узлов животных, величина группы животных
+    val animal = Animal(animalName, hostPort, "/zoo",
+      partySize.toInt)
+    // Код взаимодействия с ZooKeeper
+    try {
+      // Связь объекта с ZooKeeper, создание
+      //эфимерного узела с именем animalName и подписываться на обновления
+      //группы /zoo
+      animal.enter()
+      println(s"${animal.name} entered.")
+      // Cообщение о работе процесса
+      for (i <- 1 to Random.nextInt(100)) {
+        Thread.sleep(sleepTime)
+        println(s"${animal.name} is running...")
+      }
+      animal.leave()
+
+    } catch {
+      case e: Exception => println("Animal was not permitted to the zoo." + e)
+    }
+  }
+}
